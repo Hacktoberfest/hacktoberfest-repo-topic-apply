403d0672bb88166ce3776166206bf05e9aefe949
Mason Egger <mason@masonegger.com>
2020-10-02 16:26:13 -0500

    init commit

---
+++ go.mod
@@ -0,0 +1,16 @@
+module github.com/do-community/do-ansible-inventory
+
+go 1.14
+
+require (
+	github.com/alecthomas/template v0.0.0-20190718012654-fb15b899a751 // indirect
+	github.com/alecthomas/units v0.0.0-20190924025748-f65c72e2690d // indirect
+	github.com/apex/log v1.1.4
+	github.com/golang/protobuf v1.3.5 // indirect
+	github.com/google/go-github/v32 v32.1.0
+	golang.org/x/net v0.0.0-20200324143707-d3edc9973b7e // indirect
+	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d
+	google.golang.org/appengine v1.6.5 // indirect
+	gopkg.in/alecthomas/kingpin.v2 v2.2.6
+	gopkg.in/yaml.v2 v2.3.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,108 @@
+/*
+Copyright 2020 Kamal Nasser All rights reserved.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package main
+
+import (
+	"context"
+	"os"
+
+	"golang.org/x/oauth2"
+
+	"github.com/apex/log"
+	"github.com/apex/log/handlers/cli"
+	"github.com/google/go-github/v32/github"
+	"gopkg.in/alecthomas/kingpin.v2"
+)
+
+var (
+	githubToken = kingpin.Flag("access-token", "GitHub API Token - if unset, attempts to use this tool's stored token of its current default context. env var: GITHUB_ACCESS_TOKEN").Short('t').Envar("GITHUB_ACCESS_TOKEN").String()
+	githubUser  = kingpin.Flag("gh-user", "github user to fetch repos of").Short('u').String()
+	githubOrg   = kingpin.Flag("gh-org", "github org to fetch repos of").Short('o').String()
+	topic       = kingpin.Flag("topic", "topic to add to repos").Default("hacktoberfest").String()
+	remove      = kingpin.Flag("remove", "Remove hacktoberfest topic from all repos").Short('r').Default("false").Bool()
+)
+
+func main() {
+	kingpin.Parse()
+	log.SetHandler(cli.Default)
+
+	if *githubToken == "" {
+		log.Info("no access token provided, attempting to look up githubs's access token in env vars")
+		token := os.Getenv("GITHUB_ACCESS_TOKEN")
+		if token == "" {
+			log.Fatalf("couldn't look up token")
+		}
+
+		*githubToken = token
+		log.Info("using github's access token found in env vars")
+	}
+
+	if *githubOrg == "" && *githubUser == "" {
+		log.Fatalf("Niether githubOrg or githubUser was set.")
+	} else if *githubOrg != "" && *githubUser != "" {
+		log.Fatalf("Both githubOrg and githubUser cannot be set")
+	}
+
+	ctx := context.Background()
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: *githubToken},
+	)
+	tc := oauth2.NewClient(ctx, ts)
+	client := github.NewClient(tc)
+
+	var repos []*github.Repository
+	var owner string
+
+	if *githubOrg != "" {
+		owner = *githubOrg
+		opt := &github.RepositoryListByOrgOptions{Type: "public"}
+		var err error
+		repos, _, err = client.Repositories.ListByOrg(ctx, owner, opt)
+		if err != nil {
+			log.WithError(err).Fatalf("issue getting repositories")
+		}
+	} else if *githubUser != "" {
+		owner = *githubUser
+		opt := &github.RepositoryListOptions{Type: "public"}
+		var err error
+		repos, _, err = client.Repositories.List(ctx, owner, opt)
+		if err != nil {
+			log.WithError(err).Fatalf("issue getting repositories")
+		}
+	}
+
+	for _, repo := range repos {
+		var operation string
+		var topics []string
+		if *remove == true {
+			operation = "removing"
+			for _, t := range repo.Topics {
+				if t != *topic {
+					topics = append(topics, t)
+				}
+			}
+		} else {
+			operation = "adding"
+			topics = repo.Topics
+			topics = append(topics, *topic)
+		}
+		_, _, err := client.Repositories.ReplaceAllTopics(ctx, owner, *repo.Name, topics)
+		log.WithField("repo", *repo.Name).WithField("topic", *topic).Infof("%s topic", operation)
+		if err != nil {
+			log.WithError(err).Fatalf("issue adding hacktoberfest label")
+		}
+	}
+
+	log.Info("done!")
+}
